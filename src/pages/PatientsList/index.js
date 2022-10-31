9537efd5c4463fdc0650f5ea0c9ab1b955c4859e
peter0512lee <peter0512lee@gmail.com>
2022-10-31 20:22:55 +0800

    update home.js and finish connect api

--- src/pages/PatientsList/index.js
+++ src/pages/PatientsList/index.js
@@ -3,31 +3,45 @@ import TopBar from "../components/TopBar";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import SendIcon from "@mui/icons-material/Send";
 import { Button } from "@mui/material";
-import list from "./data.json";
+import axios from 'axios'
 
 const columns = [
-	{ field: "id", headerName: "ID", width: 70 },
-	{ field: "name", headerName: "Name", width: 130 },
-	{ field: "age", headerName: "Age", width: 130 },
-	{ field: "sex", headerName: "Sex", width: 130 },
-	{ field: "birth", headerName: "Birth", width: 130 },
+	{ field: "id", headerName: "ID", flex: 1 },
+	{ field: "name", headerName: "Name", flex: 1 },
+	{ field: "age", headerName: "Age", flex: 1 },
+	{ field: "sex", headerName: "Sex", flex: 1 },
 ];
 
-const rows = list;
-
-
-
 export default function PatientsList() {
+	
+	const [row, setRow] = React.useState([]);
+	const [selectedID, setSelectedID] = React.useState([]);
+
+	React.useEffect(() => {
+		axios.get('http://140.114.77.34:5000/api/get_patient_list')
+			.then(res => {
+				setRow(res.data)
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	}, [])
+
+	localStorage.setItem('id', selectedID)
 
 	return (
 		<div style={{ height: 400, width: "100%" }}>
 			<TopBar />
 			<br />
 			<DataGrid 
-				rows={rows} 
+				rows={row}
 				columns={columns} 
-				pageSize={5} 
 				components={{ Toolbar: GridToolbar }}
+				onSelectionModelChange={(ids) => {
+					const selectedIds = new Set(ids);
+					const selectedRow = row.filter((row) => selectedIds.has(row.id));
+					setSelectedID(selectedRow[0].id);
+				}}
 			/>
 			<br />
 			<div style={{ display: "flex", justifyContent: "right" }}>
@@ -36,6 +50,16 @@ export default function PatientsList() {
 					color="primary"
 					endIcon={<SendIcon />}
 					href="/personal"
+					onClick={() => {
+						axios.get('http://140.114.77.34:5000/api/get_patient?id=' + selectedID)
+							.then(res => {
+								localStorage.setItem('patient', JSON.stringify(res.data))
+							})
+							.catch(err => {
+								console.log(err)
+							}
+						)
+					}}
 				>
 					Select
 				</Button>
